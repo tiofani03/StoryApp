0aa9dfc5ec84dc86aa28127df69d09f489004e44
tiofani <tiofani03@gmail.com>
2022-05-28 21:51:55 +0700

    initial commit

---
+++ app/src/main/java/com/tiooooo/storyapp/ui/LoginActivity.kt
@@ -0,0 +1,12 @@
+package com.tiooooo.storyapp.ui
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import com.tiooooo.storyapp.R
+
+class LoginActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_login)
+    }
+}
