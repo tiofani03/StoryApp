4800b4cc93019288fb76a55a63a009c3a7b04a6c
tiofani <tiofani03@gmail.com>
2022-06-18 22:15:18 +0700

    now can create story with image

--- core/src/main/java/com/tiooooo/core/contract/StoriesRepositoryContract.kt
+++ core/src/main/java/com/tiooooo/core/contract/StoriesRepositoryContract.kt
@@ -3,7 +3,12 @@ package com.tiooooo.core.contract
 import com.tiooooo.core.model.StoriesViewParam
 import com.tiooooo.core.utils.States
 import kotlinx.coroutines.flow.Flow
+import java.io.File
 
 interface StoriesRepositoryContract {
     suspend fun getStories(): Flow<States<ArrayList<StoriesViewParam>>>
+    suspend fun createStories(
+        description: String,
+        image: File
+    ): Flow<States<Boolean>>
 }
